4c7642747ca0f3e3b6fd222ddd2f3c4a9f6da182
token-hub <63064941+token-hub@users.noreply.github.com>
2020-07-26 19:40:43 +0800

    added scss file

---
+++ src/components/QuotesGenerator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class QuotesGenerator extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1> Quotes Generator </h1>
+			</div>
+		)
+	}
+}
+
+export default QuotesGenerator
